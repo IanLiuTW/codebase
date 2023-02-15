6173aaf4dc846d8241f4028988f3ae9bc99ea9c9
Ian Liu <ipuppyian@gmail.com>
2023-02-15 14:57:03 -0800

    union_find.py: update the code

--- Data Structure/union_find.py
+++ Data Structure/union_find.py
@@ -4,22 +4,20 @@ class UnionFind:
         self.rank = [1] * size
 
     def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
+        if x != self.root[x]:
+            self.root[x] = self.find(self.root[x])
         return self.root[x]
 
     def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.root[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.root[root_x] = root_y
             else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
+                self.root[root_y] = root_x
+                self.rank[root_x] += 1
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
