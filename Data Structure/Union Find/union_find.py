5d6831c3b095cefa8b3c4c138a931d0bd7fea730
Ian Liu <ipuppyian@gmail.com>
2023-02-19 15:10:14 -0800

    union_find: Update implementation

--- Data Structure/Union Find/union_find.py
+++ Data Structure/Union Find/union_find.py
@@ -1,25 +1,26 @@
+# Disjoint Set Data Structure with Rank and Path Compression
+
 class UnionFind:
     def __init__(self, size):
-        self.root = [i for i in range(size)]
-        self.rank = [1] * size
+        self.root = list(range(size))
+        self.rank = [0] * size
 
     def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
         return self.root[x]
 
     def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
+        x_set, y_set = self.find(x), self.find(y)
+        if x_set == y_set:
+            return
+        if self.rank[x_set] > self.rank[y_set]:
+            self.root[y_set] = x_set
+        elif self.rank[x_set] < self.rank[y_set]:
+            self.root[x_set] = y_set
+        else:
+            self.root[y_set] = x_set
+            self.rank[x_set] += 1
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
